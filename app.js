849a6aca02467cc4d7c84dcf93c12f2a363935c1
sagaino <sagaino18@gmail.com>
2022-04-03 02:23:08 +0700

    asd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require("cors");
 
 const authRouter = require("./app/api/auth/router");
 const productRouter = require("./app/api/product/router");
@@ -10,7 +9,6 @@ const URL = `/api/v1`;
 
 const app = express();
 
-app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
