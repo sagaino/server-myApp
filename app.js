0ccfcc7cf68cdb36238fda3d5b4e8ee6ace5c7a3
sagaino <sagaino18@gmail.com>
2022-04-03 23:33:16 +0700

    ss

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ const productRouter = require("./app/api/product/router");
 const URL = `/api/v1`;
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(logger("dev"));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
