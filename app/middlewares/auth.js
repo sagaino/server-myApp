311d007bda184a0052c724d1896b077791a1abde
sagaino <sagaino18@gmail.com>
2022-04-04 01:23:36 +0700

    dd

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -3,13 +3,21 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   auth: (req, res, next) => {
     try {
-      const decoded = jwt.verify(req.headers.token, "secret");
+      const token = req.headers.authorization
+        ? req.headers.authorization.replace("Bearer ", "")
+        : null;
+      const decoded = jwt.verify(token, "secret");
       if (decoded) {
         req.user = decoded.user;
+        req.token = token;
         next();
+      } else {
+        throw new Error();
       }
     } catch (err) {
-      res.status(401).json({ message: "invalid token" });
+      res.status(401).json({
+        message: "Not authorized to access this resource",
+      });
     }
   },
 };
