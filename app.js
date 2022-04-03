a1938305940c5c91f86facb3bff700d84e144c13
sagaino <sagaino18@gmail.com>
2022-04-03 22:22:47 +0700

    1d

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const cors = require("cors");
 const authRouter = require("./app/api/auth/router");
 const productRouter = require("./app/api/product/router");
 const URL = `/api/v1`;
+
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const app = express();
 app.use(cors());
 app.use(logger("dev"));
 
